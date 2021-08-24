16dbe6bdc4df8847e16e2e1f390d95702852abf3
funn1est <admin@n1x.cn>
2021-08-25 00:32:18 +0800

    update

--- nvim/.config/nvim/lua/config/bufferline.lua
+++ nvim/.config/nvim/lua/config/bufferline.lua
@@ -33,7 +33,6 @@ require('bufferline').setup({
     enforce_regular_tabs = false,
     show_buffer_close_icons = true,
     separator_style = 'thin',
-    mappings = 'true',
     diagnostics = false,
     --diagnostics = 'nvim_lsp',
     diagnostics_indicator = function(_, _, diagnostics_dict)
