30d584d8ef320f0449c178cca1dff784e5258ea3
funn1est <admin@f1t.io>
2023-06-27 00:32:58 +0800

    update

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
   spec = {
     -- import LazyVim plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.editor.flash" },
     -- import/override with your plugins
     { import = "plugins" },
     -- import any extras modules here
